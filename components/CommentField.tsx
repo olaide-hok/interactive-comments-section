4c33d75dd65cbb76fff396d2bc76f755563a8e91
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-24 13:52:34 +0100

    save comment to memory

--- components/CommentField.tsx
+++ components/CommentField.tsx
@@ -1,7 +1,17 @@
 import Image from 'next/image';
 import Button from './Button';
+import {useState} from 'react';
+import {useCommentsStore} from '@/store';
 
 const CommentField = () => {
+    const {createComment} = useCommentsStore();
+    const [commentText, setCommentText] = useState('');
+
+    const handleSendComment = () => {
+        createComment(commentText.trim());
+        setCommentText('');
+    };
+
     return (
         <div className="flex flex-col md:flex-row items-start justify-center gap-y-(--sp-200) gap-x-(--sp-200) bg-(--clr-white) p-(--sp-200) md:p-(--sp-300) rounded-lg w-full lg:w-[45.6rem]">
             <Image
@@ -16,7 +26,9 @@ const CommentField = () => {
                 name="comment"
                 id="comment"
                 className="resize-none text-(--clr-grey-800) placeholder:text-(--clr-grey-500) text-(length:--fs-16) leading-(--lh-150) px-(--sp-200) py-(--sp-100) border border-(--clr-grey-100) focus:outline-(--clr-purple-600) rounded-lg w-full lg:w-[31.625rem] min-h-[6rem] caret-(--clr-purple-600)"
-                placeholder="Add a comment..."></textarea>
+                placeholder="Add a comment..."
+                value={commentText}
+                onChange={(e) => setCommentText(e.target.value)}></textarea>
             <div className="flex items-center justify-between w-full md:hidden">
                 {/* Avatar */}
                 <Image
@@ -28,10 +40,20 @@ const CommentField = () => {
                 />
 
                 {/* Send Button */}
-                <Button className="" name="Send" variant="reply" />
+                <Button
+                    className=""
+                    name="Send"
+                    variant="reply"
+                    onClick={handleSendComment}
+                />
             </div>
             {/* Send Button */}
-            <Button className="hidden md:block" name="Send" variant="reply" />
+            <Button
+                className="hidden md:block"
+                name="Send"
+                variant="reply"
+                onClick={handleSendComment}
+            />
         </div>
     );
 };
