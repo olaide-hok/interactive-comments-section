9817db1edcfd22e14aaba46f0053a2a4dc7a634b
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-24 15:15:04 +0100

    create CommentCardReplyField component

---
+++ components/CommentCardReplyField.tsx
@@ -0,0 +1,74 @@
+import Image from 'next/image';
+import Button from './Button';
+import {useState} from 'react';
+import {useCommentsStore} from '@/store';
+
+interface CommentCardReplyFieldProps {
+    commentId: number;
+    setShowCommentReplyField: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CommentCardReplyField = ({
+    commentId,
+    setShowCommentReplyField,
+}: CommentCardReplyFieldProps) => {
+    const {replyComment} = useCommentsStore();
+    const [commentText, setCommentText] = useState('');
+
+    const handleReplyComment = () => {
+        if (!commentText) {
+            setShowCommentReplyField(false);
+            return;
+        }
+        replyComment(commentId, commentText.trim());
+        setCommentText('');
+        setShowCommentReplyField(false);
+    };
+
+    return (
+        <div className="flex flex-col md:flex-row items-start justify-center gap-y-(--sp-200) gap-x-(--sp-200) bg-(--clr-white) p-(--sp-200) md:p-(--sp-300) rounded-lg w-full lg:w-[45.6rem]">
+            <Image
+                className="hidden md:block"
+                src="/avatars/image-juliusomo.png"
+                alt="avatar"
+                height={40}
+                width={40}
+            />
+            {/* TextArea */}
+            <textarea
+                name="comment"
+                id="comment"
+                className="resize-none text-(--clr-grey-800) placeholder:text-(--clr-grey-500) text-(length:--fs-16) leading-(--lh-150) px-(--sp-200) py-(--sp-100) border border-(--clr-grey-100) focus:outline-(--clr-purple-600) rounded-lg w-full lg:w-[31.625rem] min-h-[6rem] caret-(--clr-purple-600)"
+                placeholder="Add a comment..."
+                value={commentText}
+                onChange={(e) => setCommentText(e.target.value)}></textarea>
+            <div className="flex items-center justify-between w-full md:hidden">
+                {/* Avatar */}
+                <Image
+                    className=""
+                    src="/avatars/image-juliusomo.png"
+                    alt="avatar"
+                    height={32}
+                    width={32}
+                />
+
+                {/* Send Button */}
+                <Button
+                    className=""
+                    name="Reply"
+                    variant="reply"
+                    onClick={handleReplyComment}
+                />
+            </div>
+            {/* Send Button */}
+            <Button
+                className="hidden md:block"
+                name="Reply"
+                variant="reply"
+                onClick={handleReplyComment}
+            />
+        </div>
+    );
+};
+
+export default CommentCardReplyField;
