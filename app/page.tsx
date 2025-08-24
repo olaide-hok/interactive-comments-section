4f43f04f44c261eb5987642cce6fa7f9ab5fa721
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-24 11:49:35 +0100

    render DeleteCard modal based on deleteModalOpen boolean state value

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 'use client';
 
 import CommentCard from '@/components/CommentCard';
+import CommentField from '@/components/CommentField';
+import DeleteCard from '@/components/DeleteCard';
 import {useCommentsStore} from '@/store';
 
 export default function Home() {
-    const {comments} = useCommentsStore();
+    const {comments, deleteModalOpen} = useCommentsStore();
 
     return (
-        <div className="flex justify-self-center items-center justify-center py-[2rem] px-(--sp-200) lg:py-[3.62rem] w-[90rem] ">
-            <div className=" flex flex-col justify-center gap-y-(--sp-200) md:gap-y-(--sp-300)">
+        <div className="flex flex-col justify-self-center items-center justify-center py-[2rem] px-(--sp-200) lg:py-[3.62rem] lg:w-[90rem]">
+            <div className=" flex flex-col justify-center gap-y-(--sp-200) md:gap-y-(--sp-300) lg:w-[45.6rem]">
                 {comments.map((comment) => (
                     <CommentCard key={comment.id} comment={comment} />
                 ))}
             </div>
+            {/* Comment Field */}
+            <CommentField />
+            {/* Delete Card */}
+            {deleteModalOpen && <DeleteCard />}
         </div>
     );
 }
