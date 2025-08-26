1e76c273677676d7f6a6f190ff0bf3c9de3531be
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-26 18:17:34 +0100

    pull off extra state variables from useCommentsStore and pass to CommentCard component.

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,20 @@ import CommentField from '@/components/CommentField';
 import {useCommentsStore} from '@/store';
 
 export default function Home() {
-    const {comments} = useCommentsStore();
+    const {comments, incCommentScore, decCommentScore, user} =
+        useCommentsStore();
 
     return (
         <div className="flex flex-col justify-self-center items-center justify-center py-[2rem] px-(--sp-200) lg:py-[3.62rem] lg:w-[90rem]">
             <div className=" flex flex-col justify-center gap-y-(--sp-200) md:gap-y-(--sp-300) lg:w-[45.6rem]">
                 {comments.map((comment, index) => (
-                    <CommentCard key={comment.id + index} comment={comment} />
+                    <CommentCard
+                        key={comment.id + index}
+                        comment={comment}
+                        incScore={incCommentScore}
+                        decScore={decCommentScore}
+                        user={user.username}
+                    />
                 ))}
             </div>
             {/* Comment Field */}
